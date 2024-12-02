1fd119622ff8fac183a7843cacb6f87ed32ae75b
Bertrand <bertranddungan@gmail.com>
2024-12-02 21:09:54 +1000

    Part 2 done

--- Program.cs
+++ Program.cs
@@ -2,5 +2,8 @@
 using AdventOfCode2024.Week1;
 
 var input = InputParsing.LoadTextResource("Week1\\Input.txt");
-var result = Week1.GetTotalDistance(input);
-Console.WriteLine(result);
+var part1Result = Week1.GetTotalDistance(input);
+var part2Result = Week1.GetSimilarityScore(input);
+
+Console.WriteLine($"Part 1: {part1Result}");
+Console.WriteLine($"Part 2: {part2Result}");
--- Week1/Week1.cs
+++ Week1/Week1.cs
@@ -13,14 +13,34 @@ public static partial class Week1
 
     private static IEnumerable<(int First, int Second)> FindPairs(string input)
     {
-        var group1 = FirstHalf().Matches(input).Select(match => int.Parse(match.Value)).OrderBy(value => value);
-        var group2 = SecondHalf().Matches(input).Select(match => int.Parse(match.Value)).OrderBy(value => value);
+        var group1 = FirstHalf().Matches(input).Select(match => int.Parse(match.Value)).OrderDescending();
+        var group2 = SecondHalf().Matches(input).Select(match => int.Parse(match.Value)).OrderDescending();
         return group1.Zip(group2);
     }
 
     private static int GetPairDifference((int First, int Second) input)
     {
         return Math.Abs(input.First - input.Second);
+    }
+
+    public static int GetSimilarityScore(string input)
+    {
+        var group1 = FirstHalf().Matches(input).Select(match => int.Parse(match.Value)).Distinct();
+        var group2 = SecondHalf().Matches(input).Select(match => int.Parse(match.Value));
+
+        var similarityTracker = group1.ToDictionary(value => value, _ => 0);
+
+        foreach (var number in group2)
+        {
+            if (similarityTracker.ContainsKey(number))
+            {
+                similarityTracker[number] += 1;
+            }
+        }
+
+        var similarityScoreSum = similarityTracker.Select(pair => pair.Key * pair.Value).Sum();
+
+        return similarityScoreSum;
     }
 
     [GeneratedRegex("([0-9]+) ")]
