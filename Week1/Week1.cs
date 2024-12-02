2ead0b1e15468fb4e6efc6db8f82187562a07299
Bertrand <bertranddungan@gmail.com>
2024-12-02 20:43:40 +1000

    Tidy up part one

--- Week1/Week1.cs
+++ Week1/Week1.cs
@@ -2,7 +2,7 @@ using System.Text.RegularExpressions;
 
 namespace AdventOfCode2024.Week1;
 
-public static class Week1
+public static partial class Week1
 {
     public static int GetTotalDistance(string input)
     {
@@ -13,16 +13,19 @@ public static class Week1
 
     private static IEnumerable<(int First, int Second)> FindPairs(string input)
     {
-        var group1 = Regex.Matches(input, Group1Pattern).Select(match => int.Parse(match.Value)).OrderBy(value => value);
-        var group2 = Regex.Matches(input, Group2Pattern).Select(match => int.Parse(match.Value)).OrderBy(value => value);
+        var group1 = FirstHalf().Matches(input).Select(match => int.Parse(match.Value)).OrderBy(value => value);
+        var group2 = SecondHalf().Matches(input).Select(match => int.Parse(match.Value)).OrderBy(value => value);
         return group1.Zip(group2);
     }
-
-    private const string Group1Pattern = "([0-9]+) ";
-    private const string Group2Pattern = " ([0-9]+)";
 
     private static int GetPairDifference((int First, int Second) input)
     {
         return Math.Abs(input.First - input.Second);
     }
+
+    [GeneratedRegex("([0-9]+) ")]
+    private static partial Regex FirstHalf();
+
+    [GeneratedRegex(" ([0-9]+)")]
+    private static partial Regex SecondHalf();
 }
