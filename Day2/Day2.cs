3fe3af8462e6884e977e3b37e33e72f704f0a722
Bertrand <bertranddungan@gmail.com>
2024-12-05 00:16:47 +1000

    Rough day 2 - part 1

--- Day2/Day2.cs
+++ Day2/Day2.cs
@@ -1,6 +1,36 @@
 namespace AdventOfCode2024.Day2;
 
-public class Day2
+public static class Day2
 {
-    
+    public static int GetSafeReports(string input)
+    {
+        var reports = GetReportLevels(input);
+        var safeReports = reports.Where(ReportIsSafe);
+        return safeReports.Count();
+    }
+
+    private static IEnumerable<IEnumerable<int>> GetReportLevels(string reports)
+    {
+        return reports.Split('\n').Select(report => report.Split(' ').Select(int.Parse));
+    }
+
+    private static bool ReportIsSafe(IEnumerable<int> report)
+    {
+        var comparisonLevel = report.First();
+        var levelSign = Math.Sign(comparisonLevel - report.ElementAt(1));
+
+        foreach (var level in report.Skip(1))
+        {
+            var levelDifference = Math.Abs(comparisonLevel - level);
+            var levelSignDiff = Math.Sign(comparisonLevel - level);
+            if (levelDifference > 3 || levelDifference == 0 || levelSign != levelSignDiff)
+            {
+                return false;
+            }
+
+            comparisonLevel = level;
+        }
+
+        return true;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
 ﻿using AdventOfCode2024.Helpers;
-using AdventOfCode2024.Day1;
+using AdventOfCode2024.Day2;
 
-var input = InputParsing.LoadTextResource("Day1\\Input.txt");
-var part1Result = Day1.GetTotalDistance(input);
-var part2Result = Day1.GetSimilarityScore(input);
+var input = InputParsing.LoadTextResource("Day2\\Input.txt");
+var part1Result = Day2.GetSafeReports(input);
 
 Console.WriteLine($"Part 1: {part1Result}");
-Console.WriteLine($"Part 2: {part2Result}");
