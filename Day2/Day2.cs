253ff86c6bd52de2d236921d8be5cec99b730b77
Bertrand <bertranddungan@gmail.com>
2024-12-05 22:24:22 +1000

    Finish part 2 of day 2

--- Day2/Day2.cs
+++ Day2/Day2.cs
@@ -5,7 +5,14 @@ public static class Day2
     public static int GetSafeReports(string input)
     {
         var reports = GetReportLevels(input);
-        var safeReports = reports.Where(ReportIsSafe);
+        var safeReports = reports.Where(ReportIsCompletelySafe);
+        return safeReports.Count();
+    }
+
+    public static int GetSafeReportsWithOneMistake(string input)
+    {
+        var reports = GetReportLevels(input);
+        var safeReports = reports.Where(ReportIsTolerablySafe);
         return safeReports.Count();
     }
 
@@ -14,7 +21,7 @@ public static class Day2
         return reports.Split('\n').Select(report => report.Split(' ').Select(int.Parse));
     }
 
-    private static bool ReportIsSafe(IEnumerable<int> report)
+    private static bool ReportIsCompletelySafe(IEnumerable<int> report)
     {
         var comparisonLevel = report.First();
         var levelSign = Math.Sign(comparisonLevel - report.ElementAt(1));
@@ -32,5 +39,37 @@ public static class Day2
         }
 
         return true;
+    }
+
+    private static bool ReportIsTolerablySafe(IEnumerable<int> report)
+    {
+        var comparisonLevel = report.First();
+        var levelSign = Math.Sign(comparisonLevel - report.ElementAt(1));
+
+        foreach (var level in report.Skip(1))
+        {
+            var levelDifference = Math.Abs(comparisonLevel - level);
+            var levelSignDiff = Math.Sign(comparisonLevel - level);
+            if (levelDifference > 3 || levelDifference == 0 || levelSign != levelSignDiff)
+            {
+                var alternativePermutations = GetReportPermutations(report);
+                return alternativePermutations.Any(ReportIsCompletelySafe);
+            }
+
+            comparisonLevel = level;
+        }
+
+        return true;
+    }
+
+    private static IEnumerable<IEnumerable<int>> GetReportPermutations(IEnumerable<int> report)
+    {
+        var permutations = new List<IEnumerable<int>>();
+        for (var currentIndex = 0; currentIndex < report.Count(); currentIndex++)
+        {
+            permutations.Add(report.Where((_, comparisonIndex) => comparisonIndex != currentIndex).ToList());
+        }
+
+        return permutations;
     }
 }
